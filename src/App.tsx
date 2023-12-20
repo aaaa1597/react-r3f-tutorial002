4d240b7f1fa034deecb0a0018506978d0efd2fe2
jun <jun@Ubt2200.aaaaa>
2023-12-20 22:35:06 +0900

    コンポーネント化とpropsの例

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,28 @@ import React from 'react';
 import './App.css';
 import { Canvas } from '@react-three/fiber'
 
-function App() {
+type Props = {
+  position?: number[];
+  name?: string;
+}
+
+const Box = (props: Props) => {
+  return (
+    <mesh {...props}>
+      <boxGeometry />
+      <meshBasicMaterial color={0x00ff00} wireframe />
+    </mesh>
+  )
+}
+
+const App = () => {
   return (
     <div style={{ width: "75vw", height: "75vh" }}>
-      <Canvas>
+      <Canvas camera={{ position: [3, 1, 2] }}>
         <ambientLight />
         <directionalLight />
-        <mesh>
-          <boxGeometry args={[4, 4, 4]}/>
-          <meshBasicMaterial color={0x00ff00} wireframe />
-        </mesh>
+        <Box position={[-0.75, 0, 0]} name="A" />
+        <Box position={[0.75, 0, 0]} name="B" />
       </Canvas>
     </div>
   );
